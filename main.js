d5cc400e33d854075df7d9f1a00a51ab9b1d57bb
zohaib hassan <144324040+zohiabhassan@users.noreply.github.com>
2024-03-23 07:52:17 +0500

    created a file

---
+++ main.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+const answer = await inquirer.prompt([
+    {
+        message: "pleas guess a number:",
+        type: "number",
+        name: "userGussedNumber",
+    },
+]);
+if (answer.userGussedNumber === randomNumber) {
+    console.log("congtratulations! you choose right number");
+}
+else {
+    console.log("you choose wrong number!");
+}
